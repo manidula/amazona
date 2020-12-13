46f83639d98e2fc004bfd90674fe1e1d1421d94c
Amanuel <manuelkenna@gmail.com>
2020-12-13 13:53:04 -0500

    Adding Backend Server
    Adding Node Js Backend Server

---
+++ Backend/Data.js
@@ -0,0 +1,90 @@
+const Data = {
+  products: [
+    {
+      _id: "1",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p1.jpg",
+      price: 75,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      isInStock: true,
+      stockCount: 10,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+    {
+      _id: "2",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p2.jpg",
+      price: 85,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 21,
+      isInStock: true,
+      stockCount: 10,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+    {
+      _id: "3",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p3.jpg",
+      price: 86,
+      brand: "Nike",
+      rating: 4,
+      numReviews: 4,
+      isInStock: true,
+      stockCount: 6,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+    {
+      _id: "4",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p4.jpg",
+      price: 87,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      isInStock: false,
+      stockCount: 0,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+    {
+      _id: "5",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p5.jpg",
+      price: 88,
+      brand: "Nike",
+      rating: 2.5,
+      numReviews: 8,
+      isInStock: true,
+      stockCount: 9,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+    {
+      _id: "6",
+      name: 'Nike ACG "Gift Shop"',
+      category: "Long-Sleeve Top",
+      image: "/images/p6.jpg",
+      price: 89,
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 15,
+      isInStock: false,
+      stockCount: 20,
+      description:
+        'The Nike ACG "Gift Shop" Top wears like a souvenir from your favorite landmark. It reps Oregon\'s Smith Rock with sweat-wicking technology to help you stay dry. This product is made with at least 75% recycled polyester fibers.',
+    },
+  ],
+};
+
+export default Data;
---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Data from "./Data.js";
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(Data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.listen(5000, () => {
+  console.log("Server at http://localhost: 5000");
+});
